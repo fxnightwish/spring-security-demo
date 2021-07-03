3be8367ee0f6435af5e1c0fcd5483fbe173e52a3
FengXin <fxnightwish@outlook.com>
2021-07-03 13:03:31 +0800

    初始哈

---
+++ security-hello/src/test/java/com/frankie/org/securityhello/SecurityHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankie.org.securityhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
